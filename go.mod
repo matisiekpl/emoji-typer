0aea96ef79a9cd071ab8b8f392fbc2ff003d74f7
Mateusz Woźniak <matisiek11@gmail.com>
2021-11-24 18:44:10 +0100

    Added controllers for chat

--- go.mod
+++ go.mod
@@ -2,4 +2,17 @@ module emoji_typer
 
 go 1.16
 
-require github.com/labstack/echo/v4 v4.6.1 // indirect
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/aymerick/raymond v2.0.2+incompatible // indirect
+	github.com/forPelevin/gomoji v0.0.0-20210718160015-5fcf0e405128 // indirect
+	github.com/gobuffalo/envy v1.10.1 // indirect
+	github.com/gobuffalo/packr v1.30.1 // indirect
+	github.com/gobuffalo/packr/v2 v2.8.3 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/labstack/echo/v4 v4.6.1 // indirect
+	github.com/markbates/grift v1.0.1 // indirect
+	github.com/tidwall/gjson v1.11.0 // indirect
+	github.com/valyala/fasthttp v1.31.0 // indirect
+	golang.org/x/sys v0.0.0-20211123173158-ef496fb156ab // indirect
+)
--- main.go
+++ main.go
@@ -1,12 +1,57 @@
 package main
 
 import (
+	"github.com/aymerick/raymond"
+	"github.com/forPelevin/gomoji"
+	"github.com/gobuffalo/packr/v2"
 	"github.com/labstack/echo/v4"
+	"net/http"
 	"os"
 )
 
+type Message struct {
+	Author  string
+	Content string
+}
+
+var messages []Message
+var box *packr.Box
+
+func getEmojiString(list []gomoji.Emoji) string {
+	output := ""
+	for _, emoji := range list {
+		output = output + emoji.Character
+	}
+	return output
+}
+
+func insert(c echo.Context) error {
+	message := Message{
+		Author:  getEmojiString(gomoji.FindAll(c.FormValue("author"))),
+		Content: getEmojiString(gomoji.FindAll(c.FormValue("content"))),
+	}
+	if len(message.Author) == 0 || len(message.Content) == 0 {
+		return c.HTML(http.StatusOK, "Z pewnego powodu, jedynie emotki są dostępne w tym forum 😎 <a href=\"/\">Cofnij</a>")
+	}
+	messages = append([]Message{message}, messages...)
+	return c.Redirect(http.StatusSeeOther, "/")
+}
+
+func index(c echo.Context) error {
+	tpl, _ := box.FindString("index.html")
+	html, _ := raymond.Render(tpl, map[string]interface{}{
+		"messages": messages,
+	})
+	return c.HTML(http.StatusOK, html)
+}
+
 func main() {
+	box = packr.New("Templates", "./templates")
+
 	e := echo.New()
+	e.GET("/", index)
+	e.POST("/insert", insert)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
